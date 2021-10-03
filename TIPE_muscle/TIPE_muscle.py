fbf53cfec96606450c084e1a7f826fcc1f4dbbb9
yanis merryjoke <yanis.jouanaud@gmail.com>
2021-10-03 17:58:58 +0200

    début...

---
+++ TIPE_muscle/TIPE_muscle.py
@@ -0,0 +1,16 @@
+class fil :
+    
+    active = 0
+    def __init__(self, longueur, resistance):
+        self.L=longueur
+        self.res=resistance
+
+class muscle:
+    
+    active = 0
+    def __init__(self, taille, bulles, expansion):
+        self.size = taille
+        self.bubbles = bulles
+        self.exp = expansion
+
+
