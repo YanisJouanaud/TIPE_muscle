198fdfab179bafbfd997147fe4ecc65059c68748
yanis merryjoke <yanis.jouanaud@gmail.com>
2021-10-17 18:16:23 +0200

    Bon du coup j'ai mis la resistance thermique sus crack donc on gagne plus de 100 degrés au total, mais on gonfle toujours d'au plus quelques diziemes de millimetres, donc à voir

--- TIPE_muscle/bulle_naive.py
+++ TIPE_muscle/bulle_naive.py
@@ -1,22 +1,31 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from animate_bulle import anim_expansion
 #On considère ici une bulle constituée d'un gaz parfait dans un solide elastique
 #  chauffée à même la résistance et dont la perte calorifique suit la loi de
 #  refroidissement de Newton.
 
 
+
 def rtoV(r):
     return 4/3*np.pi*r**3
 
 def init():
-    return {"n":1.6*(10**-7),"Rs":0.5,"Rth":6.25,"I":1,"C":2092*0.04,"Text":300,"r_ini":0.001,"P_ext":10**5,"Mod_Young":0.5*10**6}
+    return {"n":5*(10**-3),"Rs":1,"Rth":90,"I":1,"C":2092*0.1,"Text":293,"r_ini":0.001,"P_ext":10**5,"Mod_Young":0.5*10**6}
 
-def pression(t, V) :
+def f(x,T):
     dic=init()
-    n,Rs,Rth,I,C,Text = dic["n"], dic["Rs"],dic["Rth"],dic["I"],dic["C"],dic["Text"]
-    return n * 8.314 * (Rs * Rth * (I ** 2) * (1 - np.exp(-(1 / Rth * C) * t)) + Text) / V
+    return -dic["P_ext"]*x**3-2*dic["Mod_Young"]*x**2+3*dic["n"]*8.314*T/(4*np.pi)
 
+def Df(x) :
+    dic=init()
+    return -3*dic["P_ext"]*x**2-4*dic["Mod_Young"]*x
+
+def temperature(t) :
+    dic=init()
+    Rs,Rth,I,C,Text = dic["Rs"],dic["Rth"],dic["I"],dic["C"],dic["Text"]
+    return (Rs * Rth * (I ** 2) * (1 - np.exp(-(1 / Rth * C) * t)) + Text)
+    
 def radius(P, r):
     dic=init()
     n,Pext, Mod_Young = dic["n"], dic["P_ext"], dic["Mod_Young"]
@@ -26,11 +35,23 @@ def radius(P, r):
         P=n*R*T/((4/3)*np.pi*r**3)
     return r
 
+def newton(f, df, T, ini, epsilon) : #prend un parametre T en plus des parametres usuels
+    x=ini
+    while abs(f(x, T))>epsilon :
+        if df(x)!=0 :
+            x=x-f(x,T)/(df(x))
+        else :
+            x=x-f(x,T)/(df(x)-10**(-6))
+    return x
+
 def expansion(t): #t doit ici être un linspace
-    r=[init()["r_ini"]]
+    dic=init()
+    epsilon=10**(-6)
+    r=[newton(f, Df, dic["Text"], 0, epsilon)]
     for i in t:
-        r.append(radius(pression(i, rtoV(r[-1])), r[-1]))
-        print(pression(i, rtoV(r[-1])), "Pascals")
+        temp=temperature(i)
+        r.append(newton(f, Df, temp, r[-1], epsilon))
+        print(temp)
     return r
 
 def get_radius_expansion(t):
@@ -40,5 +61,5 @@ def get_radius_expansion(t):
     else :
         return expansion(t)
 
-print(expansion(np.linspace(0,100,100000)))
-#anim_expansion(get_radius_expansion)
+print(expansion(np.linspace(0,10,1000)))
+anim_expansion(get_radius_expansion)
